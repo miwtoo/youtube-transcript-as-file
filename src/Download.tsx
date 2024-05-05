6578f4750dad4b1f51e9e812960c26bff5481e8e
miwtoo <miwyugioh@gmail.com>
2024-05-05 17:20:12 +0700

    style: :lipstick: add styling tailwind

--- src/Download.tsx
+++ src/Download.tsx
@@ -36,7 +36,12 @@ const Download: React.FC<DownloadProps> = (props) => {
     };
 
     return (
-        <button onClick={handleDownload}>
+        <button onClick={handleDownload} className="inline-flex items-center gap-x-2 px-3 py-1 shadow-lg shadow-gray-500/50 bg-black text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97]">
+            <span className="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
+                </svg>
+            </span>
             Download
         </button>
     );
